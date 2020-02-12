1ad1ef6a6d69ebf93a28a4672ce2d0de43a3dfa1
Archie Baer <archie@abaer.dev>
2020-02-12 16:58:05 +0000

    Converts image to png

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const template = fs.readFileSync(__dirname + "/template.svg", "utf8").split("[x]");
+const {convert} = require("convert-svg-to-png");
 
 //Express
 const express = require("express");
@@ -7,7 +8,8 @@ const app = express();
 app.listen(8012);
 
 //ID
-app.get("/:id", (req, res) => {
+app.get("/:id", async (req, res) => {
     const svg = template[0] + Math.floor(Math.random() * 1000) + template[1];
-    res.send(svg);
+    const png = await convert(svg);
+    res.type("image/png").send(png);
 });
